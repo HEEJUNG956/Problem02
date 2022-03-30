d8803258f3a9159d838526671f0e32a5160bdff1
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-03-31 08:47:02 +0900

    문자열내림차순

---
+++ 2페이지 level 1 18번 - 문자열내림차순/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2페이지_level_1_18번___문자열내림차순
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            char[] a = s.ToCharArray();
+            Array.Sort(a);
+            Array.Reverse(a);
+
+        }
+    }
+}
