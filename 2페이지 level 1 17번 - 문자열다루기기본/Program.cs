c6739d8980201f5eca372a146859df4cf12b2cb5
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-03-30 17:08:06 +0900

    문자열다루기기본

---
+++ 2페이지 level 1 17번 - 문자열다루기기본/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2페이지_level_1_17번___문자열다루기기본
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool answer = false;
+            string s = Console.ReadLine();
+            if(s.Length == 4 || s.Length == 6)
+            {
+                answer = int.TryParse(s, out int i);
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
