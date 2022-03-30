6672bd2a5a7d4e37712c83665c64608ba36a088e
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-03-30 16:57:30 +0900

    김서방찾기

---
+++ 2페이지 level 1 16번 - 서울에서김서방찾기/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2페이지_level_1_16번___서울에서김서방찾기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            string answer = "";
+            string[] seoul = {a, b};
+            
+
+            for(int i = 0; i < seoul.Length; i++)
+            {
+                if(seoul[i] == "kim")
+                {
+                    answer = "김서방은 " + i + "에 있다";
+                }
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
