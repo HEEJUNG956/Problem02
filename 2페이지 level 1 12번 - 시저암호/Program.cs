f019dd9629b5427cd78e06d97ad5b2cf9b64f26a
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-03-30 16:37:46 +0900

    시저암호

---
+++ 2페이지 level 1 12번 - 시저암호/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2페이지_level_1_12번___시저암호
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+
+            string answer = "";
+            char[] temp = s.ToCharArray();
+            for(int i = 0; i < s.Length; i++)
+            {
+                if ((temp[i] >= 'a' && temp[i] <= 'z') || (temp[i] >= 'A' && temp[i] <= 'Z'))
+                {
+                    if(char.IsUpper(temp[i]))
+                    {
+                        temp[i] = (char)((temp[i] + n - 'A') % 26 + 'A');
+                    }
+                    else
+                    {
+                        temp[i] = (char)((temp[i] + n - 'a') % 26 + 'a');
+                    }
+                }
+            }
+            answer = new string(temp);
+            Console.WriteLine(answer);
+        }
+    }
+}
