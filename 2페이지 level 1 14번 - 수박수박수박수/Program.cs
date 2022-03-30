1c2aa841e107fd51cc2070812c158a5fd9fa7242
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-03-30 16:44:22 +0900

    수박수박수

---
+++ 2페이지 level 1 14번 - 수박수박수박수/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2페이지_level_1_14번___수박수박수박수
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string answer = "";
+
+            for(int i = 0 ; i < n; i++)
+            {
+                answer += i % 2 == 0 ? "수" : "박";
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
