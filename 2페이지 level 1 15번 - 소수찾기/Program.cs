919fb21a3032363791d4a9c95538b26942597c8c
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-03-30 16:52:05 +0900

    소수찾기

---
+++ 2페이지 level 1 15번 - 소수찾기/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2페이지_level_1_15번___소수찾기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int answer = 0;
+            bool[] arr = new bool[n + 1];
+
+            for(int i = 2; i < n + 1; i++)
+            {
+                if(!arr[i])
+                {
+                    answer++;
+                    for(int j = 0; i * j < n + 1; j++)
+                    {
+                        arr[i * j] = true;
+                    }
+                }
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
