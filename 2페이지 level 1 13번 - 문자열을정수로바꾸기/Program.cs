dea49581a4fec32b875f3374686364528f1d7d85
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-03-30 16:40:34 +0900

    문자열을정수로바꾸기

---
+++ 2페이지 level 1 13번 - 문자열을정수로바꾸기/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2페이지_level_1_13번___문자열을정수로바꾸기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int answer = int.Parse(s);
+            Console.WriteLine(answer);
+        }
+    }
+}
