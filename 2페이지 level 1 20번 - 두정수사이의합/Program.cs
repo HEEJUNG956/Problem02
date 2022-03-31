32b967a98cead77840378779eab65921d0fffda6
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-03-31 09:51:46 +0900

    두정수사이의합

---
+++ 2페이지 level 1 20번 - 두정수사이의합/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2페이지_level_1_20번___두정수사이의합
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            long answer = 0;
+
+            while(a != b)
+            {
+                answer += a;
+                a = (a > b) ? a - 1 : a + 1;
+            }
+            Console.WriteLine(answer + b);
+        }
+    }
+}
