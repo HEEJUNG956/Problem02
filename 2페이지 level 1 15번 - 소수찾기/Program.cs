5e2440e28d0fe8e028568edc35db092792ae859d
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-03-31 10:17:51 +0900

    소수찾기 수정

--- 2페이지 level 1 15번 - 소수찾기/Program.cs
+++ 2페이지 level 1 15번 - 소수찾기/Program.cs
@@ -13,14 +13,17 @@ namespace _2페이지_level_1_15번___소수찾기
             int n = int.Parse(Console.ReadLine());
             int answer = 0;
             bool[] arr = new bool[n + 1];
+            // 모두 false
 
             for(int i = 2; i < n + 1; i++)
             {
+                // 10 입력시 i 2부터 11까지 반복
                 if(!arr[i])
                 {
                     answer++;
                     for(int j = 0; i * j < n + 1; j++)
                     {
+                        // 
                         arr[i * j] = true;
                     }
                 }
