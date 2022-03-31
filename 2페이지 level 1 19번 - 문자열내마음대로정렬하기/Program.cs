6d7fd479ce73c8fd75151d32853f8adf59400319
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-03-31 09:43:34 +0900

    문자열내마음대로정렬하기

---
+++ 2페이지 level 1 19번 - 문자열내마음대로정렬하기/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2페이지_level_1_19번___문자열내마음대로정렬하기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = { Console.ReadLine() };
+            int n = int.Parse(Console.ReadLine());
+            string[] answer = strings.OrderBy(x => x).ToArray();
+            answer = answer.OrderBy(x => x).ToArray();
+
+            Console.WriteLine(answer);
+        }
+    }
+}
